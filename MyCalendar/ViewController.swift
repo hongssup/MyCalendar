aec54653a320b7b354fcb19b5c60a304af402829
hongssup <hongssup0202@gmail.com>
2021-03-21 23:42:22 +0900

    Initial Commit

---
+++ MyCalendar/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MyCalendar
+//
+//  Created by SeoYeon Hong on 2021/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .white
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
