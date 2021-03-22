8255591f8cdeaf215a0055a15718947ce7e36d25
hongssup <hongssup0202@gmail.com>
2021-03-22 21:43:15 +0900

    Add FSCalendar

--- MyCalendar/ViewController.swift
+++ MyCalendar/ViewController.swift
@@ -6,18 +6,41 @@
 //
 
 import UIKit
+import FSCalendar
 
 class ViewController: UIViewController {
+    
+    let calendar = FSCalendar()
     
     override func loadView() {
         super.loadView()
         view.backgroundColor = .white
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupCalendar()
+        addConstraints()
     }
-
-
+    
+    func setupCalendar() {
+        calendar.dataSource = self
+        calendar.delegate = self
+        view.addSubview(calendar)
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func addConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 12),
+            calendar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 4),
+            calendar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -4),
+            calendar.heightAnchor.constraint(equalToConstant: 400)
+        ])
+    }
 }
 
+extension  ViewController: FSCalendarDelegate, FSCalendarDataSource {
+    
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'MyCalendar' do
+  
+  use_frameworks!
+  pod 'FontAwesome.swift'
+  pod 'FSCalendar'
+
+end
